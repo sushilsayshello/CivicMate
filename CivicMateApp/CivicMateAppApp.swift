ccd4704a05b687161f4445464cf811e286ed057d
Sushil Shakya <sushilsayshello@gmail.com>
2024-09-08 14:10:00 +1000

    Initial Commit

---
+++ CivicMateApp/CivicMateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CivicMateAppApp.swift
+//  CivicMateApp
+//
+//  Created by admin on 8/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct CivicMateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
